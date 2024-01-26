aa09cf5319fff7162ac95dd664a0c8e48f6e8a5a
Davvi <95988082+DavIsmael@users.noreply.github.com>
2024-01-26 02:24:26 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose number to verify if it's prime or not ");
+        int number = scanner.nextInt();
+        boolean isPrime;
+        //1, 2, and 3 - 1 is not prime, but 2 and 3 are!
+        //If number equals 2 or greater than 2, then it sets it to prime.
+        if(number < 2) isPrime = false;
+        else isPrime = true;
+        //Checks up to square root of 'number' to optimize for loop by reducing the number of iterations
+        for(int i = 2; i <= number / i; ++i){
+            //Division remainder has to be 0 to be a composite number, if not, it's prime
+            if(number % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println("The number " + number + " is prime!");
+        }else if(number == 1){
+            System.out.println(number + " isnt prime nor is it a composite number because it only has 1 factor!");
+        }else{
+            System.out.println("The number " + number + " isnt prime, therefore it's a composite number!");
+        }
+    }
+}
